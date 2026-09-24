c63c6a91826f5c726019d81a11683b1992597f11
shoheikanaya-wq <shoheikanaya@gmail.com>
2026-09-25 02:21:22 +0900

    Refresh cache for v3.12.45

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kor-practice-v59';
-const SHELL=['./','./index.html','./fixed-audio.js?v=312440','./reply-guide.js?v=312440','./lessons.js?v=312440','./lessons-extra.js?v=312440','./manifest.webmanifest?v=312440','./icons/icon-192.png','./icons/icon-512.png','./practice-tools.js?v=312440','./today-summary.js?v=312440'];
+const CACHE='kor-practice-v60';
+const SHELL=['./','./index.html','./fixed-audio.js?v=312450','./reply-guide.js?v=312450','./lessons.js?v=312450','./lessons-extra.js?v=312450','./manifest.webmanifest?v=312450','./icons/icon-192.png','./icons/icon-512.png','./practice-tools.js?v=312450','./today-summary.js?v=312450'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('kor-practice-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 async function networkFirst(request){try{const response=await fetch(request,{cache:'no-store'});if(response&&response.ok){const copy=response.clone();const cache=await caches.open(CACHE);await cache.put(request,copy)}return response}catch(err){return(await caches.match(request))||(request.mode==='navigate'?await caches.match('./index.html'):Response.error())}}
