ba80dfb5abe38715c09093751bc79205f6e05608
shoheikanaya-wq <shoheikanaya@gmail.com>
2026-09-25 01:19:43 +0900

    Refresh cache for clear A/B Korean voice labels

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='kor-practice-v46';
-const SHELL=['./','./index.html','./lessons.js?v=312310','./lessons-extra.js?v=312310','./manifest.webmanifest?v=312310','./icons/icon-192.png','./icons/icon-512.png','./practice-tools.js?v=312310','./today-summary.js?v=312310'];
+const CACHE='kor-practice-v47';
+const SHELL=['./','./index.html','./lessons.js?v=312320','./lessons-extra.js?v=312320','./manifest.webmanifest?v=312320','./icons/icon-192.png','./icons/icon-512.png','./practice-tools.js?v=312320','./today-summary.js?v=312320'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key.startsWith('kor-practice-')&&key!==CACHE).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))});
 async function networkFirst(request){try{const response=await fetch(request,{cache:'no-store'});if(response&&response.ok){const copy=response.clone();const cache=await caches.open(CACHE);await cache.put(request,copy)}return response}catch(err){return(await caches.match(request))||(request.mode==='navigate'?await caches.match('./index.html'):Response.error())}}
